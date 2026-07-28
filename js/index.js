eafc41b4d9ed724d5e8e264dd9d2c69386b3bc8b
Motlatsi <166697983+Babies02@users.noreply.github.com>
2026-07-28 11:04:17 +0200

    Update text content in index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something to tell ya momma'.split('')
+const text = 'I Have Something for ya momma'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
